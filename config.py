7222be953604a0528ca9503e8474bd4871438f8c
Nayeem223 <125780682+Nayeem223@users.noreply.github.com>
2026-07-21 20:06:57 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+CHANNELS = [
+    int(os.getenv("CHANNEL_1")),
+    int(os.getenv("CHANNEL_2")),
+    int(os.getenv("CHANNEL_3")),
+    int(os.getenv("CHANNEL_4")),
+]
